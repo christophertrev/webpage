6bc5177eba9412d322f256dd787f13bc25da9b59
Christopher Treviño <christophertrev@gmail.com>
2015-02-14 15:08:49 -0800

    (fix) Link should redirect now

--- client/src/views/projectView.js
+++ client/src/views/projectView.js
@@ -1,6 +1,6 @@
 var ProjectView = Backbone.View.extend({
 
-   template: _.template(' <a href="<%= link =%>" class="image"><img src="<%= imgSRC %>" alt="" /></a>'+
+   template: _.template(' <a href="<%= link %>" class="image"><img src="<%= imgSRC %>" alt="" /></a>'+
     '<div class="inner">' +
       '<h4><%= title %></h4>' +
       '<p><%= description%></p>' +
