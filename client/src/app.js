5d989e4decf2cd161cf148687a5c5fad2c472c9f
Christopher Treviño <christophertrev@gmail.com>
2015-02-14 13:33:12 -0800

    (Feat) Add projects to page

--- client/src/app.js
+++ client/src/app.js
@@ -1,15 +1,7 @@
 $(function(){
   
-  // var messages = new Messages();
-  // messages.fetchMessages();
-  // view = new MessagesView({el: $('.chatWindow'), collection: messages})
-  // view.render();
-  // view = new ProjectView({el:$('.features')})
   var features = new FeatureProjects([{},{},{}]);
   var projectViews = new ProjectsView({collection: features})
-
-  // var project = new ProjectModel();
-  // var view = new ProjectView({model: project})
-  // $('.features').append(view.render());
+  $('.projects').append(projectViews.render());
 
 });
--- client/src/views/featuresView.js
+++ client/src/views/featuresView.js
@@ -1,9 +1,15 @@
 var ProjectsView = Backbone.View.extend({
-  tagName: 'feature',
+  // tagName: 'div',
   initialize: function(){
     console.log(this.collection)
+    this.$el.addClass('features')
   },
   render: function(){
-
+    var a = this.collection.map(function(project){
+      // console.log(project)
+      return new ProjectView({model: project}).render();
+    })
+    console.log(a)
+    return this.$el.append(a)
   }
 })
