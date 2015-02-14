078671f6fd6ad9756ed83317f74803c6dd3521d7
Christopher Treviño <christophertrev@gmail.com>
2015-02-13 19:57:05 -0800

    (feat) Saving work for the day

--- client/src/app.js
+++ client/src/app.js
@@ -5,10 +5,11 @@ $(function(){
   // view = new MessagesView({el: $('.chatWindow'), collection: messages})
   // view.render();
   // view = new ProjectView({el:$('.features')})
-  var project = new ProjectModel();
-  var view = new ProjectView({model: project})
-  $('.features').append(view.render());
+  var features = new FeatureProjects([{},{},{}]);
+  var projectViews = new ProjectsView({collection: features})
 
-
+  // var project = new ProjectModel();
+  // var view = new ProjectView({model: project})
+  // $('.features').append(view.render());
 
 });
---
+++ client/src/views/featuresView.js
@@ -0,0 +1,9 @@
+var ProjectsView = Backbone.View.extend({
+  tagName: 'feature',
+  initialize: function(){
+    console.log(this.collection)
+  },
+  render: function(){
+
+  }
+})
