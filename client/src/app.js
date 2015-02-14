5c4f3405a1207dd1dce7d5508278d49a8710bfb5
Christopher Treviño <christophertrev@gmail.com>
2015-02-14 13:41:54 -0800

    (Feat) add articles into app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,37 @@
 $(function(){
   
-  var features = new FeatureProjects([{},{},{}]);
+  var projects = [
+  {
+    title: 'Stroll Health - Patient Portal',
+    imgSRC: 'images/pic01.jpg',
+    description: 'A web application that allows users to brain storm anonymously',
+    link: '#'
+  },
+  {
+    title: "'strom",
+    imgSRC: 'images/storm.png',
+    description: 'A web application that allows users to brain storm anonymously',
+    link: 'http://unstable-asteroid.azurewebsites.net/'
+  },
+  {
+    title: 'Drawn Together',
+    imgSRC: 'images/drawnLogo.png',
+    description: 'A game that allows user to draw head to head against a friend',
+    link: 'https://fast-plateau-6652.herokuapp.com/'
+  },
+  ]
+
+
+  var features = new FeatureProjects(projects);
   var projectViews = new ProjectsView({collection: features})
   $('.projects').append(projectViews.render());
 
 });
+
+// <article>
+//   <a href="#" class="image"><img src="images/pic01.jpg" alt="" /></a>
+//   <div class="inner">
+//     <h4>Stroll Health - Patient Portal</h4>
+//     <p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer adipiscing ornare amet.</p>
+//   </div>
+// </article>
--- client/src/models/projectModel.js
+++ client/src/models/projectModel.js
@@ -2,6 +2,7 @@ var ProjectModel = Backbone.Model.extend({
   defaults: {
     'imgSRC': 'images/pic01.jpg',
     'description': 'Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer adipiscing ornare amet.',
-    'title': 'Stroll Health - Patient Portal'
+    'title': 'Stroll Health - Patient Portal',
+    'link': '#'
   }
 })
